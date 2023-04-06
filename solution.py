fd67a74f4fd6ec005613b788bd3c4c71ab5459e4
Элина Новикова <118816624+ElinaNovikova@users.noreply.github.com>
2023-04-06 15:24:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,11 +7,12 @@ from scipy.stats import norm
 chat_id = 1112920502 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    from scipy.stats import t
+    mu = 1/2 - np.exp(1) # смещение распределения
+    var = (1/2 - np.exp(1))**2 # дисперсия распределения
+    alpha = 1 - p
     n = len(x)
-    X_ = np.mean(x)
-    s = np.sqrt(np.sum((x - X_)**2) / (n - 1))
-    t_p = t.ppf(1 - p/2, n - 1)
-    delta = t_p * s / np.sqrt(n)
-    return (X_ - delta, X_ + delta)
+    t_stat = abs(norm.ppf(alpha/2)) * np.sqrt(n-1)/np.sqrt(n) # статистика для распределения Стьюдента
+    loc = 2*x.mean()/(5**2) - mu # мат. ожидание коэффициента ускорения
+    scale = np.sqrt(var) / np.sqrt(n)
+    return loc - t_stat*scale, loc + t_stat*scale
     
